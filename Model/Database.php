5150b3e7285d726af3191a6ed05e2d6c082d86c7
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-05 11:30:21 +0000

    added a Database class to Database.php, so we should be able to get the database connection

--- Model/Database.php
+++ Model/Database.php
@@ -1,1 +1,30 @@
 <?php
+class Database{
+    private $_dbHandle;
+
+    private static $_dbInstance;
+
+    private function __construct(){
+        try{
+            $this->_dbHandle = new PDO('sqlite:bcs.sqlite');
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+
+    private static function getInstance(){
+        if(self::$_dbInstance == null){
+            self::$_dbInstance = new Database();
+        }
+        return self::$_dbInstance;
+    }
+
+    public function getConnection(){
+        return $this->_dbHandle;
+    }
+
+    public function __destruct(){
+        $this->_dbHandle = null;
+    }
+}
