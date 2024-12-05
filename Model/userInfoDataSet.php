203790ffe54f70d6f642943830e49d299b9f97ce
shabbeer9891 <s.ahmed139@edu.salford.ac.uk>
2024-12-05 14:29:35 +0000

    userInfo classes updated

--- Model/userInfoDataSet.php
+++ Model/userInfoDataSet.php
@@ -1,5 +1,12 @@
 <?php
+require_once ('Model/Database.php');
+require_once ('Model/userInfoDataSet.php');
 
 class userInfoDataSet{
+    protected $dbHandle, $dbInstance;
 
+    public function __construct(){
+        $this->dbInstance = Database::getInstance();
+        $this->dbHandle = $this->dbInstance->getDbConnection();
+    }
 }
