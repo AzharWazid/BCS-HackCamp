31d15aaf687ddaba9daf4691d0efa61b64ceeeb4
hannaan456 <h.ahmed41@edu.salford.ac.uk>
2024-12-06 21:25:09 +0000

    made register page

--- Controller/registerPage1.php
+++ Controller/registerPage1.php
@@ -1,3 +1,18 @@
 <?php
 // Include the login view (HTML form)
 require("../View/registerPage1.phtml");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $email = $_POST['email'];
+    $dob = $_POST['dob'];
+    $phone = $_POST['phone'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $userTypes = isset($_POST['user_type']) ? $_POST['user_type'] : [];
+
+    // Process the data (e.g., validation, saving to the database)
+    // Example:
+    // echo "User Registered: $firstName $lastName with email $email";
+}
