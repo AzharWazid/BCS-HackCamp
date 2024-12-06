4ca8db288761e519393c5b634b55963976a9011d
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-06 11:18:05 +0000

    Made some progress on uploading CV

--- Model/studentInfoDataSet.php
+++ Model/studentInfoDataSet.php
@@ -10,5 +10,25 @@ class studentInfoDataSet{
         $this->dbHandle = $this->dbInstance->getDbConnection();
     }
 
+    public function storeStudentCV($userId, $cv){
+        //checks for errors
+        if($cv['error'] != UPLOAD_ERR_OK){
+            return 'upload error';
+        }
 
+        //ensures the file type is pdf
+        $validFileExtensions = ['pdf'];
+        if(!in_array($cv["type"], $validFileExtensions)){
+            return 'type error';
+        }
+
+        //path to user folder
+        $uploadDir = 'uploads/user_' . $userId;
+        //checks if user folder exists
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+
+    }
 }
