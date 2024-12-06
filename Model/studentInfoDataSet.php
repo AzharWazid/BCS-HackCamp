af7b1d62b686db43faca2b49d875484f9e2ea7f2
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-06 12:00:36 +0000

    Made some more progress on uploading CV

--- Model/studentInfoDataSet.php
+++ Model/studentInfoDataSet.php
@@ -17,18 +17,26 @@ class studentInfoDataSet{
         }
 
         //ensures the file type is pdf
-        $validFileExtensions = ['pdf'];
+        $validFileExtensions = ['application/pdf'];
         if(!in_array($cv["type"], $validFileExtensions)){
             return 'type error';
         }
 
         //path to user folder
-        $uploadDir = 'uploads/user_' . $userId;
+        $uploadDir = 'CV Uploads/user_' . $userId;
         //checks if user folder exists
         if (!is_dir($uploadDir)) {
             mkdir($uploadDir, 0777, true);
         }
 
+        $fileName = uniqid('cv_') . '.pdf';
+        $filePath = $uploadDir . '/' . $fileName;
 
+        if(move_uploaded_file($cv['tmp_name'], $filePath)){
+            return $filePath;
+        }
+        else{
+            return 'dir upload error';
+        }
     }
 }
