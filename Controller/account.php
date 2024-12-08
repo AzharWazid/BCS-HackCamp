de5aef6f72c9343ac0a71c67687c9d81c63c01fc
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-08 13:32:14 +0000

    add dropdown change in header when user is logged in. add a logout func logout.php and added it in the header

---
+++ Controller/account.php
@@ -0,0 +1,4 @@
+<?php
+
+
+require_once ("../View/account.phtml");
---
+++ Controller/logout.php
@@ -0,0 +1,14 @@
+<?php
+// logout.php
+session_start(); // Start the session
+
+// Unset all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page or home page
+header("Location: ../index.php");
+exit;
+?>
