18e6d23676cb872d0ed81109cca24657cc817930
Liam <l.metcalfe@edu.salford.ac.uk>
2024-12-09 12:26:00 +0000

    account.php started

--- Controller/account.php
+++ Controller/account.php
@@ -1,5 +1,15 @@
 <?php
 
-echo 'hello';
+require_once("../Model/userInfoDataSet.php");
+$view = new stdClass();
+$UserInfoDataSet = new UserInfoDataSet();
+$view->userInfoDataSet=$UserInfoDataSet->getUserInfo($_SESSION["id"]);
+
+if ($_SESSION["userType"] == "2")
+{
+    $studentInfoDataSet = new StudentInfoDataSet();
+    $view->studentInfoDataSet=$studentInfoDataSet->getStudentInfo($view->userInfoDataSet->getID());
+
+}
 
 require_once ("../View/account.phtml");
