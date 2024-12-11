b5417c0b813e6c6a716997d3adaa5a8b493e7707
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-11 16:02:26 +0000

    fixed error message when not signed in on placements.php

--- Controller/placements.php
+++ Controller/placements.php
@@ -17,18 +17,21 @@ $view = new stdClass();
 $jobListData = new jobListDataSet();
 $skill = new Skill();
 // Fetch Data for logged in user
+if(!isset($_SESSION['id'])){
+    $_SESSION['id'] = null;
+}
 $view->jobListData = $jobListData->getJobListData($_SESSION['id']);
 $view->skill = $skill->skillAssign();
 //var_dump($view->skill[1]);
 
 // Check user type to control data visibility
-if ($_SESSION['userType'] == "2")
+if (isset($_SESSION['userType']) && $_SESSION['userType'] == "2")
 {
     $jobListData = new jobListDataSet();
     $view->jobListDataSet = $jobListData->getJobListData($_SESSION['id']);
 
 }
-elseif ($_SESSION['userType'] == "1" || $_SESSION['userType'] == "3")
+elseif (isset($_SESSION['userType']) && ($_SESSION['userType'] == "1" || $_SESSION['userType'] == "3"))
 {
     $view->jobListDataSet = $jobListData;
 }
