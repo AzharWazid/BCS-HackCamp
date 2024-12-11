6c99c59a619212b9c5301181bb8dc5c2f950e322
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-11 14:50:28 +0000

    made edits to editDetails.php

--- Controller/editDetails.php
+++ Controller/editDetails.php
@@ -4,6 +4,17 @@ require_once("../Model/studentInfoDataSet.php");
 
 session_start();
 
+$view = new stdClass();
+$UserInfoDataSet = new UserInfoDataSet();
+$view->userInfoDataSet = $UserInfoDataSet->getUserInfo($_SESSION["id"]);
+
+if ($_SESSION["userType"] == "2") {
+    $studentInfoDataSet = new StudentInfoDataSet();
+    if ($studentInfoDataSet->validateStudentInfo($view->userInfoDataSet->getId()) >= 1) {
+        $view->studentInfoDataSet = $studentInfoDataSet->getStudentInfo($view->userInfoDataSet->getID());
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userInfoDataSet = new userInfoDataSet();
 
@@ -15,34 +26,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_SESSION['id'];
 
     // Update in database
-    $dbInstance = Database::getInstance();
-    $dbHandle = $dbInstance->getDbConnection();
 
-    $SQL1 = "UPDATE UserInfo SET address = :address, phoneNumber = :phone, dobYMD = :dob WHERE userID = :userId";
-    $stmt1 = $dbHandle->prepare($SQL1);
-    $stmt1->bindParam(':address', $address);
-    $stmt1->bindParam(':phone', $phone);
-    $stmt1->bindParam(':dob', $dob);
-    $stmt1->bindParam(':userId', $userId);
-    $stmt1->execute();
-
-    $SQL2 = "UPDATE User SET email = :email WHERE ID = :userId";
-    $stmt2 = $dbHandle->prepare($SQL2);
-    $stmt2->bindParam(':email', $email);
-    $stmt2->bindParam(':userId', $userId);
-    $stmt2->execute();
 }
 
 // load current data (can we re use load function?)
-$view = new stdClass();
-$UserInfoDataSet = new UserInfoDataSet();
-$view->userInfoDataSet = $UserInfoDataSet->getUserInfo($_SESSION["id"]);
 
-if ($_SESSION["userType"] == "2") {
-    $studentInfoDataSet = new StudentInfoDataSet();
-    if ($studentInfoDataSet->validateStudentInfo($view->userInfoDataSet->getId()) >= 1) {
-        $view->studentInfoDataSet = $studentInfoDataSet->getStudentInfo($view->userInfoDataSet->getID());
-    }
-}
+
+$dbInstance = Database::getInstance();
+$dbHandle = $dbInstance->getDbConnection();
+
+$SQL1 = "UPDATE UserInfo SET address = :address, phoneNumber = :phone, dobYMD = :dob WHERE userID = :userId";
+$stmt1 = $dbHandle->prepare($SQL1);
+$stmt1->bindParam(':address', $address);
+$stmt1->bindParam(':phone', $phone);
+$stmt1->bindParam(':dob', $dob);
+$stmt1->bindParam(':userId', $userId);
+$stmt1->execute();
+
+$SQL2 = "UPDATE User SET email = :email WHERE ID = :userId";
+$stmt2 = $dbHandle->prepare($SQL2);
+$stmt2->bindParam(':email', $email);
+$stmt2->bindParam(':userId', $userId);
+$stmt2->execute();
 
 require_once ("../View/editDetails.phtml");
