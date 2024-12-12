f85e18f7bd451e8ac5bebea0691770f039adb9e9
Liam <l.metcalfe@edu.salford.ac.uk>
2024-12-12 11:46:23 +0000

    myListings.php created

---
+++ Controller/myListings.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../Model/userInfoDataSet.php");
+require_once("../Model/studentInfoDataSet.php");
+
+session_start();
+
+$view = new stdClass();
+$UserInfoDataSet = new UserInfoDataSet();
+$view->userInfoDataSet = $UserInfoDataSet->getUserInfo($_SESSION["id"]);
+
+if ($_SESSION["userType"] == "3")
+{
+    $jobListDataSet = new JobListDataSet();
+    if ($jobListDataSet->getJobListData($view->userInfoDataSet->getID()) >= 1){
+        $view->jobListDataSet = $jobListDataSet->getJobListData($view->userInfoDataSet->getID());
+
+    }
+}
+
+require_once ("../View/myListingsView.php");
