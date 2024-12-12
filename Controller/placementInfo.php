bfe3513f49a6e49283695c20bc84d6af99386973
Liam <l.metcalfe@edu.salford.ac.uk>
2024-12-12 10:58:57 +0000

    placementinfo linking

--- Controller/placementInfo.php
+++ Controller/placementInfo.php
@@ -1,1 +1,26 @@
 <?php require_once('../View/placementInfo.phtml');
+
+session_start();
+
+$view = new stdClass();
+$jobListData = new jobListDataSet();
+$skill = new Skill();
+// Fetch Data for logged in user
+if(!isset($_SESSION['id']))
+{
+    $_SESSION['id'] = null;
+}
+$view->jobListData = $jobListData->getJobListData($_SESSION['id']);
+$view->skill = $skill->skillAssign();
+//var_dump($view->skill[1]);
+
+// Check user type to control data visibility
+if (isset($_SESSION['userType']) && $_SESSION['userType'] == '2')
+{
+    $jobListData = new jobListDataSet();
+    $view->jobListDataSet = $jobListData->getJobListData($_SESSION['id']);
+}
+elseif (isset($_SESSION['userType']) && ($_SESSION['userType'] == '1'|| $_SESSION['userType'] == '3'))
+{
+        $view->jobListDataSet = $jobListData;
+}
