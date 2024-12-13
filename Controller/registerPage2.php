917cce9b42372063f2a2c7012ac90d324796df87
Azhar-Wazid <150937368+Azhar-Wazid@users.noreply.github.com>
2024-12-13 15:03:12 +0000

    fixed error in myListings.php

--- Controller/registerPage2.php
+++ Controller/registerPage2.php
@@ -1,4 +1,10 @@
 <?php
+require_once ("../Model/studentInfoDataSet.php");
 
+$view = new stdclass();
+$studentInfoData = new studentInfoDataSet();
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+}
 require_once("../View/registerPage2.phtml");
