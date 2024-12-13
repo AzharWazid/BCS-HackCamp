58dda1bbcf2b15e8e35b8738e533ad807d7d2e8a
Liam <l.metcalfe@edu.salford.ac.uk>
2024-12-13 11:07:36 +0000

    make viewApplicants pages

--- Controller/viewApplicants.php
+++ Controller/viewApplicants.php
@@ -1,2 +1,11 @@
 <?php
 require_once('../View/viewApplicants.phtml');
+
+session_start();
+
+$view = new stdClass();
+$studentInfoData = new studentInfoDataSet();
+$skill = new Skill();
+
+$view->studentInfoData = $studentInfoData->getStudentInfo();
+$view->
